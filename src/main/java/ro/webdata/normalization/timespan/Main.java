785232f84306806ab8e99bf120c2434457837527
Ilie Cristian Dorobat <idorobat@cisco.com>
2021-01-25 02:13:33 +0200

    Updating the Main file

--- src/main/java/ro/webdata/normalization/timespan/Main.java
+++ src/main/java/ro/webdata/normalization/timespan/Main.java
@@ -2,6 +2,6 @@ package main.java.ro.webdata.normalization.timespan;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Timespan Normalization 1.0");
+        System.out.println("Timespan Normalization 1.1");
     }
 }
